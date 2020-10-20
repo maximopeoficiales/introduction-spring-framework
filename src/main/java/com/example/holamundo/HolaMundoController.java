7929eb02a045d0406af345c17898626c791c8ce4
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2020-10-20 03:22:19 -0500

    introducion a thymeleaft.org

--- src/main/java/com/example/holamundo/HolaMundoController.java
+++ src/main/java/com/example/holamundo/HolaMundoController.java
@@ -1,16 +1,44 @@
 package com.example.holamundo;
 
+import java.util.ArrayList;
+
+import com.example.domain.Persona;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
+// import lombok.extern.slf4j.Slf4j;
 
-@RestController
-@Slf4j //con esta notacion puedo depurar
+@Controller
+// @Slf4j
 public class HolaMundoController {
+     @Value("${index.saludo}") // obtengo las variables de application.properties
+     private String saludo;
+
      @GetMapping("/")
-     public String home() {
-          log.info("hola desde backend");
-          return "Hola mundo con Spring 2";
+     public String home(Model model) {
+          // log.info("Mostrando pagina de inicio");
+          Persona persona = new Persona();
+          persona.setNombre("Maximo");
+          persona.setApellido("Apaza");
+          persona.setEmail("maximopeoficiales@gmail.com");
+          persona.setTelefono("8099544");
+          // Persona 2
+          Persona persona2 = new Persona();
+          persona2.setNombre("Mayrin");
+          persona2.setApellido("Apaza");
+          persona2.setEmail("maximopeoficiales1@gmail.com");
+          persona2.setTelefono("8136666");
+          // lista de personas
+          // List<Persona> personas = Arrays.asList(persona, persona2);
+          ArrayList<Persona> personas = new ArrayList<Persona>();
+          String mensaje = "Mensaje desde Backend";// se puede declarar como var en la version 10
+          model.addAttribute("mensaje", mensaje);
+          model.addAttribute("saludo", saludo);
+          // model.addAttribute("persona", persona);
+          model.addAttribute("personas", personas);
+          return "index";
      }
 }
